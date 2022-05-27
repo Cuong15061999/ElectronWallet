1bd931f4df368d80720693c6bdf0861767933753
Cuong15061999 <phamvqcuong99@gmail.com>
2022-05-27 21:07:32 +0700

    more note

--- models/user.js
+++ models/user.js
@@ -13,8 +13,8 @@ var userSchema =new mongoose.Schema({
 
     role: String, //admin and user
     newUser: Number,//0 la` user moi, 1 la` user cu~
-    failCount: Number,//Login that bai 3 lan` se~ bi 'Dang nhap bat thuong', 3 lan` sai nua se bi 'Vo Hieu Hoa' 
-    actStatus: String, //'Xac Minh', 'Cho Xac Minh','Cho Cap Nhat','Vo Hieu Hoa'
+    failCount: Number,//0,Login that bai 3 lan` se~ bi 'Dang nhap bat thuong', 3 lan` sai nua se bi 'Vo Hieu Hoa' 
+    actStatus: String, //'Xac Minh', *'Cho Xac Minh','Cho Cap Nhat','Vo Hieu Hoa'
     loginStatus: String,//'0', 'Dang nhap bat thuong'
 
     
