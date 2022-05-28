6065c24485870cb5251f278505d9a87dc49c4d8c
Cuong15061999 <phamvqcuong99@gmail.com>
2022-05-28 22:23:49 +0700

    add new note

--- models/user.js
+++ models/user.js
@@ -13,7 +13,7 @@ var userSchema =new mongoose.Schema({
     Money: Number,//=10 000 000(khoi tao.)
 
     role: String, //admin and user
-    newUser: Number,//0 la` user moi, 1 la` user cu~
+    newUser: Number,//0 la` user moi, 1 la` user cu~****
     failCount: Number,//0,Login that bai 3 lan` se~ bi 'Dang nhap bat thuong', 3 lan` sai nua se bi 'Vo Hieu Hoa' 
     actStatus: String, //'Xac Minh', *'Cho Xac Minh','Cho Cap Nhat','Vo Hieu Hoa'
     loginStatus: String,//'0', 'Dang nhap bat thuong'
