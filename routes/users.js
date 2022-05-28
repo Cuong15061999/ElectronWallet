60eaded678dfb43a8b3c1bbc56aa3f1aa885c7b7
Cuong15061999 <phamvqcuong99@gmail.com>
2022-05-28 20:41:07 +0700

    Set up for user Page

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+//DASHBOARD
 router.get('/', function(req, res, next) {
   return res.render('index')
 });
 
+//THONG TIN CHI TIET
+router.get('/', function(req, res, next) {
+  return res.render('index')
+});
+
+//Nap & Rut TIEN
+router.get('/', function(req, res, next) {
+  return res.render('index')
+});
+
+//Mua the dt
+router.get('/', function(req, res, next) {
+  return res.render('index')
+});
+
+//Chuyen tien`
+router.get('/', function(req, res, next) {
+  return res.render('index')
+});
 module.exports = router;
