cdf28c33e79193076ab2121a37b1a0f785b23834
Cuong15061999 <phamvqcuong99@gmail.com>
2022-05-31 02:03:35 +0700

    Function Send Money

--- models/tranferHistory.js
+++ models/tranferHistory.js
@@ -3,7 +3,7 @@ var tranferHistorySchema =new mongoose.Schema({
 
     idSender: String,
     idReceiver: String,
-    money: Number[10],
+    money: Number,
     createdAt: Date,//ngay` tao.
 })
 var tranferHistory = mongoose.model('TranferHistory', tranferHistorySchema)
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,121 @@
 var express = require('express');
-const { render } = require('express/lib/response');
 var router = express.Router();
 var User = require('../models/user');
 var AtmHistory = require('../models/AtmHistory')
 var AtmCard = require('../models/AtmCard')
 var PhoneCard = require('../models/PhoneCard')
-var PhoneCHistory = require('../models/PhoneCHistory');
-const phoneCHistory = require('../models/PhoneCHistory');
+var phoneCHistory = require('../models/PhoneCHistory');
+var UserOTP = require('../models/userOTP')
+var transferHistory = require('../models/tranferHistory')
+const nodemailer = require("nodemailer");
 
+const sendOtpVerificationEmail = async (email, res) => {
+  console.log(email)
+  try {
+    const otp = `${Math.floor(10000 + Math.random() * 90000)}`;
+    new UserOTP({
+      idUser: email,
+      OTP: otp,
+      createdAt: Date.now(),
+      expiredAt: Date.now() + 300000,
+    }).save(function (err) {
+      if (err) throw err;
+      console.log('Successfully saved new OTP.');
+    });
+
+    //transporter
+    let transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false,
+      service: 'Gmail',
+      auth: {
+        user: 'phamvqcuong99@gmail.com', // generated ethereal user
+        pass: 'Quoccuong_999', // generated ethereal password
+      },
+    });
+
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Server is ready to take our messages");
+      }
+    });
+
+    //mail options
+    const mailOptions = {
+      from: 'phamvqcuong99@gmail.com',
+      to: email,
+      subject: " Otp to Verify Your Email",
+      html: `<p>Here is your OTP to Send Your Transfer Money Request: <b>${otp}</b> </p>
+      <p>This code expires in <b>5 min</b> </p>`
+    }
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+
+  } catch (error) {
+    res.json({
+      status: "failed",
+      message: error.message,
+    })
+  }
+};
+
+const sendReceiverEmail = async (email, moneyReceive, moneyNow, note, sender, res) => {
+  try {
+
+    //transporter
+    let transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false,
+      service: 'Gmail',
+      auth: {
+        user: 'phamvqcuong99@gmail.com', // generated ethereal user
+        pass: 'Quoccuong_999', // generated ethereal password
+      },
+    });
+
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Server is ready to take our messages");
+      }
+    });
+
+    //mail options
+    const mailOptions = {
+      from: 'phamvqcuong99@gmail.com',
+      to: email,
+      subject: " Otp to Verify Your Email",
+      html: `<p>${sender} Have send you <b>${moneyReceive}</b> with note: ${note},and your money in bank: <b>${moneyNow}</b> </p>`
+    }
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+
+  } catch (error) {
+    res.json({
+      status: "failed",
+      message: error.message,
+    })
+  }
+};
 /* GET users listing. */
 //DASHBOARD ** Chua do thong tin vao` bang Lich su Giao Dich
 router.get('/', function (req, res, next) {
@@ -246,7 +354,7 @@ router.post('/buyCard', function (req, res, next) {
   })
 });
 
-//Chuyen tien`
+//Chuyen tien`(done)
 router.get('/transfer', function (req, res, next) {
   //Check User are Login or Not
   console.log('email user is: ' + req.session.user)
@@ -271,7 +379,7 @@ router.get('/transfer', function (req, res, next) {
     }
   });
 });
-
+//(done)
 router.post('/transfer', function (req, res, next) {
   //Check User are Login or Not
   console.log('email user is: ' + req.session.user)
@@ -280,6 +388,106 @@ router.post('/transfer', function (req, res, next) {
   }
 
   console.log(req.body)
+  User.findOne({ phone: req.body.Phone, fullname: req.body.name }, function (err, users) {
+    //console.log(users)
+    if (!users) {
+      return res.render('transfers', { msg: "Nhan Sai Thong Tin. OR Ng dung ko ton` tai." })
+    } else {
+      User.findOne({ username: req.session.user }, function (err, user2) {
+        sendOtpVerificationEmail(user2.email, res)
+      })
+      let noteSend = req.body.note
+      console.log(noteSend)
+      return res.render('OtpSendMoney', { email: users.email, money: req.body.money, note: noteSend })
+    }
+  })
+});
+//OTP Send Money (Done)
+router.get('/OtpSendMoney', function (req, res, next) {
+  //Check User are Login or Not
+  console.log('email user is: ' + req.session.user)
+  return res.render('OtpSendMoney')
+});
+//done
+router.post('/OtpSendMoney', function (req, res, next) {
+  //Check User are Login or Not
+  console.log(' username is: ' + req.session.user)
+  console.log(req.body)
+
+  User.findOne({ username: req.session.user }, function (err, users) {
+    console.log(users.email)
+    UserOTP.findOne({ idUser: users.email }, function (err, otps) {
+      if (!otps) {
+        return res.render('transfers', { msg: "fail to send mail" })
+      } else {
+        console.log('Da gui mail')
+        if (Date.parse(Date.parse(otps.expiredAt) < Date.parse(Date.now()))) {
+          console.log('you OTP already expried Please send request again')
+          UserOTP.deleteMany({ idUser: users.email },
+            function (err, docs) {
+              if (err) {
+                console.log(err)
+              }
+              else {
+                console.log("Updated Docs : ", docs);
+              }
+            })
+          return res.render('transfers')
+        } else {//neu chua het han va` otp dung
+          if (req.body.otp === otps.OTP) {
+            console.log('OTP DUNG')
+            //delete OTP
+            UserOTP.deleteMany({ idUser: users.email },
+              function (err, docs) {
+                if (err) {
+                  console.log(err)
+                }
+                else {
+                  console.log("Updated Docs : ", docs);
+                }
+              })
+            //tru tien User Gui
+            let newMoney = parseInt(users.Money) - parseInt(req.body.money)
+            User.updateOne({ username: req.session.user }, { Money: newMoney },
+              function (err, docs) {
+                if (err) {
+                  console.log(err)
+                }
+                else {
+                  console.log("Updated Sender Docs : ", docs);
+                }
+              });
+            //Cong tien User Nhan
+            User.findOne({ email: req.body.email }, function (err, receiver) {
+              let recMoney = parseInt(receiver.Money) + parseInt(req.body.money)
+              User.updateOne({ username: receiver.username }, { Money: recMoney },
+                function (err, docs) {
+                  if (err) {
+                    console.log(err)
+                  }
+                  else {
+                    console.log("Updated Receiver Docs : ", docs);
+                    //send mail tien cho ng nhan
+                    sendReceiverEmail(receiver.email, req.body.money, recMoney, req.body.note, users.fullname, res)
+                    new transferHistory({
+                      idSender: users.email,
+                      idReceiver: receiver.email,
+                      money: req.body.money,
+                      createdAt: Date.now(),
+                    }).save()
+                      .then(() => {
+                        return res.render('transfers', { msg1: 'SEND MONEY SUCCESSFULLY' })
+                      });
+                  }
+                });
+            })
+          } else {
+            return res.render('OtpSendMoney', { msg: "Nhap Sai OTP" })
+          }
+        }
+      }
+    })
+  })
 });
 
 //login (done)
