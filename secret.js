1e8bae09c8fe0b2f88d485f94b43fb165f6edd5c
Cuong15061999 <phamvqcuong99@gmail.com>
2022-05-31 19:14:27 +0700

    test

--- secret.js
+++ secret.js
@@ -6,10 +6,10 @@ module.exports = {
     saveUninitialized: true},
     mongo:{
         dev:{
-            conn:"mongodb://127.0.0.1:27017/cuoiki"
+            conn:"mongodb://127.0.0.1:27017/testImport"
         },
         product:{
-            conn:"mongodb://127.0.0.1:27017/cuoiki"
+            conn:"mongodb://127.0.0.1:27017/testImport"
         },  
    }
 }
